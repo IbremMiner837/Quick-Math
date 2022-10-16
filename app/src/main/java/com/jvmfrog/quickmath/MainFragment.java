e38e259162dd6895c44288a8d6da91d30c19f81b
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-10-16 09:57:45 +0200

    Initial commit

---
+++ app/src/main/java/com/jvmfrog/quickmath/MainFragment.java
@@ -0,0 +1,49 @@
+package com.jvmfrog.quickmath;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jvmfrog.quickmath.databinding.FragmentMainBinding;
+
+public class MainFragment extends Fragment {
+
+    private FragmentMainBinding binding;
+
+    private static final int score = 0;
+    private int mParam1;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainFragment newInstance(String param1, String param2) {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        args.putInt(param1, score);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getInt(String.valueOf(score));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+
+
+
+        return binding.getRoot();
+    }
+}
